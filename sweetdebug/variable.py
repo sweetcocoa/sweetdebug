59457b28fa1ac2aa177c5e8b0d3d0e76e1b63907
sweetcocoa <sweetcocoa@snu.ac.kr>
2024-03-06 14:59:47 +0900

    Add sweetshow

---
+++ sweetdebug/variable.py
@@ -0,0 +1,24 @@
+import typing
+
+
+def sweetshow(
+    locals_: dict, length: int = 40, exclude: typing.List[str] = ["self", "sweetshow"]
+):
+    def RecursivePrint(k, v, depth: int = 0):
+        if isinstance(v, (list, tuple)):
+            for v0 in v:
+                RecursivePrint(k=k, v=v0, depth=depth + 1)
+        else:
+            if hasattr(v, "shape"):  # if Tensor
+                strv = str(v.shape)
+                if hasattr(v, "dtype"):
+                    strv += " " + str(v.dtype)
+            else:
+                strv = str(v)
+            if len(strv) > length:
+                strv = strv[: length // 2] + strv[length // 2 :]
+            print(f"{k} : {'['*depth} {strv} {']'*depth}")
+
+    for k, v in locals_.items():
+        if k not in exclude:
+            RecursivePrint(k, v)
